cb1d7ef4d13af88c66e7102c5c428a33f631cee5
Mazdak <nazemi.works@gmail.com>
2019-11-01 11:10:59 -0700

    more on budget app

--- third/starter/app.js
+++ third/starter/app.js
@@ -24,7 +24,32 @@ const budgedController = (function() {
       inc: 0
     }
   };
+
+  function addItem(type, des, val) {
+    let newItem, ID;
+    // last el in arr
+    if (data.allItems[type].length > 0) {
+      ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+    } else {
+      ID = 0;
+    }
+    // create new item base on
+    if (type === 'exp') {
+      newItem = new Expense(ID, des, val);
+    } else if (type === 'inc') {
+      newItem = new Income(ID, des, val);
+    }
+    data.allItems[type].push(newItem);
+    return newItem;
+  }
+  return {
+    addItem,
+    test: function() {
+      console.log(data);
+    }
+  };
 })();
+//----------------------------------------------------------------------------------
 
 // UI module
 const UIController = (function() {
@@ -32,32 +57,87 @@ const UIController = (function() {
     inputType: '.add__type',
     inputDescription: '.add__description',
     inputValue: '.add__value',
-    inputButton: '.add__btn'
+    inputButton: '.add__btn',
+    incomeContainer: '.income__list',
+    expensesContainer: '.expenses__list'
   };
+
   function getInput() {
     const { inputType, inputDescription, inputValue } = DOMStrings;
     return {
       type: document.querySelector(inputType).value, // inc or exp
       description: document.querySelector(inputDescription).value,
-      value: document.querySelector(inputValue).value
+      value: parseFloat(document.querySelector(inputValue).value)
     };
   }
+
+  function addListItem(obj, type) {
+    let html, newHtml, element;
+    // create html string with placeholder
+    if (type === 'inc') {
+      element = DOMStrings.incomeContainer;
+      html =
+        '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+    } else if (type === 'exp') {
+      element = DOMStrings.expensesContainer;
+      html =
+        '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+    }
+    // replace placeholder text
+    newHtml = html.replace('%id%', obj.id);
+    newHtml = newHtml.replace('%description%', obj.description);
+    newHtml = newHtml.replace('%value%', obj.value);
+    // insert html in to DOM
+    document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+  }
+
+  function clearFields() {
+    let fieldsArr;
+    const { inputDescription, inputValue } = DOMStrings;
+    // query selector all return list rather than arr.
+    const fields = document.querySelectorAll(
+      inputDescription + ',' + inputValue
+    );
+    // conver list to arr.
+    fieldsArr = Array.prototype.slice.call(fields);
+    fieldsArr.forEach(function(current, index, array) {
+      current.value = '';
+    });
+    // back focus on first input
+    fieldsArr[0].focus();
+  }
+
   return {
     getInput,
+    addListItem,
+    clearFields,
     DOMStrings
   };
 })();
+//-----------------------------------------------------------------------------
 
 // CONTROLLER module like event -- control entire app.
 const controller = (function(bCtrl, UICtrl) {
+  let updateBudget = function() {
+    // 4. calculate budged
+    // return budget
+    // 5. display to ui
+  };
+
   // custom func
   let ctrlAddItem = function() {
     // 1. get the input data
-    let input = UICtrl.getInput();
+    let { type, description, value } = UICtrl.getInput();
     // 2. add item to budget controller
-    // 3. add the item to ui
-    // 4. calculate budged
-    // 5. display to ui
+    if (description && !isNan(value) && value > 0) {
+      let newItem = bCtrl.addItem(type, description, value);
+      // 3. add the item to ui
+      UICtrl.addListItem(newItem, type);
+      // clear fields
+      UICtrl.clearFields();
+      // calculate and update budget
+      updateBudget();
+    }
   };
 
   let setupEventListeners = function() {
