b4f7aa67896f0490f799e2c725e026d2ed276325
Mazdak <nazemi.works@gmail.com>
2019-11-11 14:48:32 +0330

    some boilerplates

--- b-red/scroller/app.js
+++ b-red/scroller/app.js
@@ -27,42 +27,37 @@ const data = [
 
 const profiles = profileIterator(data);
 
-// Call first profile
-nextProfile();
-
-// Next Event
+// next events
 document.getElementById('next').addEventListener('click', nextProfile);
 
-// Next Profile Display
+// next profile diplay
 function nextProfile() {
-  const currentProfile = profiles.next().value;
-
-  if(currentProfile !== undefined) {
+  const currProfile = profiles.next().value;
+  if (currProfile !== undefined) {
     document.getElementById('profileDisplay').innerHTML = `
       <ul class="list-group">
-        <li class="list-group-item">Name: ${currentProfile.name}</li>
-        <li class="list-group-item">Age: ${currentProfile.age}</li>
-        <li class="list-group-item">Location: ${currentProfile.location}</li>
-        <li class="list-group-item">Preference: ${currentProfile.gender} looking for ${currentProfile.lookingfor}</li>
+        <li class="list-group-item">Name: ${currProfile.name}</li>
+        <li class="list-group-item">Name: ${currProfile.age}</li>
+        <li class="list-group-item">Name: ${currProfile.location}</li>
+        <li class="list-group-item">Name: ${currProfile.gender} looking for ${currProfile.lookingfor}</li>
       </ul>
     `;
 
-    document.getElementById('imageDisplay').innerHTML = `<img src="${currentProfile.image}">`;
+    document.getElementById(
+      'imageDisplay'
+    ).innerHTML = `<img src=${currProfile.image}>`;
   } else {
-    // No more profiles
-    window.location.reload();
+    location.reload();
   }
 }
 
-// Profile Iterator
 function profileIterator(profiles) {
   let nextIndex = 0;
-
   return {
     next: function() {
-      return nextIndex < profiles.length ? 
-      { value: profiles[nextIndex++], done: false } : 
-      { done: true }
+      return nextIndex < profiles.length
+        ? { value: profiles[nextIndex++], done: false }
+        : { done: true };
     }
   };
 }
