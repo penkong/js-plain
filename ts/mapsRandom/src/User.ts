85398b35875b84f577916c848f801c4395b9e5cc
Mazdak <nazemi.works@gmail.com>
2019-11-12 16:55:09 +0330

    first project and types

---
+++ ts/mapsRandom/src/User.ts
@@ -0,0 +1,23 @@
+// you must get types for every package for let ts know to help you.
+// type definition file. @types/{lib name} from definitely type project.
+import faker from "faker";
+
+class User {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    const {
+      name: { firstName },
+      address: { latitude, longitude }
+    } = faker;
+    this.name = firstName();
+    this.location = {
+      lat: parseFloat(latitude()),
+      lng: parseFloat(longitude())
+    };
+  }
+}
