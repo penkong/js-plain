c260611be5f860fcc778827aad8a037f3380df2d
Mazdak <nazemi.works@gmail.com>
2019-11-13 15:52:05 +0330

    more on read file and use enum as type

--- ts/stats-csv/src/index.ts
+++ ts/stats-csv/src/index.ts
@@ -1,1 +1,19 @@
-console.log("object");
+import { CsvReader } from "./CsvReader";
+import { MatchResult } from "./MatchResult";
+// -----------------------------------------
+// ENUM = ENUMERATION an object that store some closely vars
+// it signals these values are closely related
+// it's type also
+// when we know all possible values.
+const reader = new CsvReader("football.csv");
+reader.read();
+
+let manUnWins = 0;
+
+for (let match of reader.data) {
+  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
+    manUnWins++;
+  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
+    manUnWins++;
+  }
+}
