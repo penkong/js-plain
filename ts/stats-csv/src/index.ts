a7bbed563b374474d6c93b3316b50b9272b4e532
Mazdak <nazemi.works@gmail.com>
2019-11-13 16:41:58 +0330

    use of generics

--- ts/stats-csv/src/index.ts
+++ ts/stats-csv/src/index.ts
@@ -5,15 +5,15 @@ import { MatchResult } from "./MatchResult";
 // it signals these values are closely related
 // it's type also
 // when we know all possible values.
-const reader = new CsvReader("football.csv");
-reader.read();
+// const reader = new CsvReader("football.csv");
+// reader.read();
 
-let manUnWins = 0;
+// let manUnWins = 0;
 
-for (let match of reader.data) {
-  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
-    manUnWins++;
-  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
-    manUnWins++;
-  }
-}
+// for (let match of reader.data) {
+//   if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
+//     manUnWins++;
+//   } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
+//     manUnWins++;
+//   }
+// }
